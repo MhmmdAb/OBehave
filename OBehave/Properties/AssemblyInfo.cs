149de677299422c90165901b02b2aba97218723b
Mohammad Abdulfatah <mohammmad@abdulfatah.net>
2012-12-10 22:37:53 +0300

    Set assembly propertiers and version number

--- OBehave/Properties/AssemblyInfo.cs
+++ OBehave/Properties/AssemblyInfo.cs
@@ -3,28 +3,25 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("OBehave")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("OBehave")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyDescription("Simple Behavior Tree with a fluent API")]
+[assembly: AssemblyCopyright("Copyright © 2012, Mohammad Bahij Abdulfatah")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
+#if DEBUG
+#   if PORTABLE_CLASS_LIBRARY
+[assembly: AssemblyConfiguration("PCL-Debug")]
+#   else
+[assembly: AssemblyConfiguration("NET35-Debug")]
+#   endif
+#else
+#   if PORTABLE_CLASS_LIBRARY
+[assembly: AssemblyConfiguration("PCL-Release")]
+#   else
+[assembly: AssemblyConfiguration("NET35-Release")]
+#   endif
+#endif
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyFileVersion("0.1.0")]
