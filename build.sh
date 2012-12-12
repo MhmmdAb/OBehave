d6d67d7f5adc8aeabbcdfedf035292642d340e95
MhmmdAb <mohammad@abdulfatah.net>
2012-12-12 09:28:27 +0300

    Fix build.sh to work on Mac OS X with Mono 2.10.x

--- build.sh
+++ build.sh
@@ -1,30 +1,33 @@
 #!/bin/sh
 xbuild_command_name="xbuild"
 
-if [type "$xbuild_command_name" > /dev/null]; then
+type "$xbuild_command_name" > /dev/null 2>&1
+if [ $? != 0 ] ; then
   echo 'Cannot find xbuild.' 1>&2
   exit 1
 fi
+
+export EnableNuGetPackageRestore=true
 
 target="$1"
 if [ "$target" = "" ] ; then
 	target="Build"
 fi
 
-xbuild /p:Configuration=Debug /p:OutPutPath=../build/mono/Debug/ /t:"$target" /v:minimal ./OBehave35/OBehave35.csproj
-if [ $? = 0 ] ; then
+xbuild /p:Configuration=Debug /p:TargetFrameworkProfile= /p:OutPutPath=../build/mono/Debug/ /t:"$target" /v:minimal ./OBehave35/OBehave35.csproj
+if [ $? != 0 ] ; then
     exit $?
 fi
 
-xbuild /p:Configuration=Release /p:OutPutPath=../build/mono/Release/ /t:"$target" /v:minimal ./OBehave35/OBehave35.csproj
-if [ $? = 0 ] ; then
+xbuild /p:Configuration=Release /p:TargetFrameworkProfile= /p:OutPutPath=../build/mono/Release/ /t:"$target" /v:minimal ./OBehave35/OBehave35.csproj
+if [ $? != 0 ] ; then
     exit $?
 fi
 
-xbuild /p:Configuration=Debug /p:OutPutPath=../build/mono-tests/Debug/ /t:"$target" /v:minimal ./OBehave/OBehave.Tests.csproj
-if [ $? = 0 ] ; then
+xbuild /p:Configuration=Debug /p:TargetFrameworkProfile= /p:OutPutPath=../build/mono-tests/Debug/ /t:"$target" /v:minimal ./OBehave.Tests/OBehave.Tests.csproj
+if [ $? != 0 ] ; then
     exit $?
 fi
 
-xbuild /p:Configuration=Release /p:OutPutPath=../build/mono-tests/Release/ /t:"$target" /v:minimal ./OBehave/OBehave.Tests.csproj
+xbuild /p:Configuration=Release /p:TargetFrameworkProfile= /p:OutPutPath=../build/mono-tests/Release/ /t:"$target" /v:minimal ./OBehave.Tests/OBehave.Tests.csproj
 exit $?
